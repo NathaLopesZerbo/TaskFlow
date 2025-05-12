0ca11dedc1421f8d1927b47c75e1db40c13bac3b
NathaLopesZerbo <nathalopeszerbo7@gmail.com>
2025-05-12 04:54:04 -0300

    Responsividade register.php

--- src/register.php
+++ src/register.php
@@ -6,73 +6,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/build.css">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <title>Login - TaskFlow</title>
+    <title>Cadastro - TaskFlow</title>
 </head>
 
 <body class="bg-fundo">
 
     <div class="w-screen h-screen fixed top-0 left-0 flex items-center justify-center bg-opacity-60">
-        <div class="flex max-w-4xl w-full h-auto bg-white rounded-xl shadow-lg overflow-hidden ">
+        <div class="flex flex-row max-sm:flex-col-reverse max-w-4xl w-full h-auto bg-white rounded-xl shadow-lg overflow-hidden m-5">
 
+            <!-- Bloco TaskFlow -->
+            
 
-            <div class="w-1/2 p-6">
-                <h1 class="text-2xl font-bold text-center mb-3 font-pessoal cor-tit-auth mr-6">ENTRAR AO TASKFLOW</h1>
+            <!-- Bloco de Cadastro -->
+            <div class="w-1/2 max-sm:w-full p-6">
+                <h1 class="text-2xl font-bold text-center mb-3 font-pessoal cor-tit-auth">CRIAR CONTA</h1>
 
                 <form class="flex flex-col" action="tarefa_controller.php?acao=register" method="POST">
-                    <label for="nome" class="sr-only">Nome:</label>
                     <input
                         id="nome"
                         type="text"
                         name="nome"
                         placeholder="Nome:"
-                        class="w-5/6 p-2 mb-3 ml-5 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs mt-5"
+                        class="w-5/6 max-w-sm mx-auto p-2 mb-3 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs mt-5"
                         required />
 
-                    <label for="email" class="sr-only">Email:</label>
                     <input
                         id="email"
                         type="email"
                         name="email"
                         placeholder="Email:"
-                        class="w-5/6 p-2 mb-3 ml-5 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs"
+                        class="w-5/6 max-w-sm mx-auto p-2 mb-3 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs"
                         required />
 
-                    <label for="senha" class="sr-only">Senha:</label>
                     <input
                         id="senha"
                         type="password"
                         name="senha"
                         placeholder="Senha:"
-                        class="w-5/6 p-2 mt-2 ml-5 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs"
+                        class="w-5/6 max-w-sm mx-auto p-2 mb-3 bg-input border rounded-lg placeholder:text-place placeholder:font-extrabold placeholder:text-xs"
                         required />
 
                     <button
                         type="submit"
-                        class="font-bold font-pessoal flex justify-center mx-auto w-2/3 bg-auth text-white py-2 rounded-2xl mt-8 ml-14 bg-botoes cursor-pointer hover:bg-principal duration-200">
+                        class="w-2/3 max-w-sm mx-auto bg-botoes text-white font-bold py-2 rounded-2xl mt-6 hover:bg-principal transition duration-200">
                         CADASTRAR
                     </button>
                 </form>
 
-
-
-                <div class="mt-8 ml-20">
-                    <p class="text-xs cor-txt-auth font-bold ml-5">
-                        Já possui uma conta? <a href="login.php" class="font-bold cursor-pointer">Clique aqui</a>
+                <div class="mt-6 text-center">
+                    <p class="text-xs cor-txt-auth font-bold">
+                        Já possui uma conta?
+                        <a href="login.php" class="font-bold cursor-pointer text-botoes underline">Clique aqui</a>
                     </p>
-
                 </div>
             </div>
-
-            <div class="w-1/2 text-white p-6 flex flex-col justify-center items-center auth bg-principal">
-                <h2 class="text-5xl font-bold mb-10 flex">
+            <div class="w-1/2 max-sm:w-full text-white p-6 flex flex-col justify-center items-center bg-principal">
+                <h2 class="text-5xl font-bold mb-10 max-sm:mb-0 flex items-center gap-3">
                     <img src="../src/img/logo_task_flow.png" class="w-14">
                     TaskFlow
                 </h2>
             </div>
+
         </div>
     </div>
-
-
 
 </body>
 
